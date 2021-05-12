ae300beb9bfab5d00ec1066efc44e13a487a81cc
John Ryan <jryan@team.nxlink.com>
2021-05-11 22:55:00 -0500

    added MDB to project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Princigration Landing Page is under Maintenance. Please Check Back Later
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
