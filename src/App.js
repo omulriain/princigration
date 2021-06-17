0c154214c93e5a5a13bf29b5132092f9fe91d207
Jesse Sites <jesse318s@gmail.com>
2021-06-17 15:57:01 -0500

    added experience and myproject content/code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './App.css';
 import Header from "./header";
 import Main from "./main";
+import Experience from "./experience";
+import MyProjects from "./myProjects";
+import ContactMe from "./contactMe";
 import Message from "./message";
 function App() {
   return (
       <>
     <Header />
     <Main />
+    <Experience />
+    <MyProjects/>
+    <ContactMe />
     <Message />
       </>
 
--- src/experience.jsx
+++ src/experience.jsx
@@ -8,90 +8,111 @@ function Experience(){
             <hr className="my-5"/>
 
 
-            <section>
-
-
-                <div className="row">
-                    <div className="col-lg-6 mb-4 mb-lg-0 pb-2 pb-lg-0 pb-xl-2">
-                        <h5 className="text-uppercase mb-5">My experience</h5>
-                        <figure className="border-left border-primary ps-3 py-2 mb-4">
-                            <blockquote className="blockquote">
-                                <h5 className="pb-3">
-                                    <i className="fas fa-briefcase me-3" aria-hidden="true"></i>Senior developer
-                                </h5>
-                            </blockquote>
-                            <figcaption className="blockquote-footer mb-0">
-                                <p className="font-weight-bold text-body mb-2">July, 2013 - Present (4
-                                    years)</p>
-                                <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </figcaption>
-                        </figure>
-                        <figure className="border-left border-primary ps-3 py-2 mb-4">
-                            <blockquote className="blockquote">
-                                <h5 className="pb-3">
-                                    <i className="fas fa-briefcase me-3" aria-hidden="true"></i>JavaScript
-                                    developer
-                                </h5>
-                            </blockquote>
-                            <figcaption className="blockquote-footer mb-0">
-                                <p className="font-weight-bold text-body mb-2">
-                                    March 2011 - June 2012 (1 year, 3 Month)
-                                </p>
-                                <p className="mb-0">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quas, eos
-                                    officia maiores ipsam ipsum dolores.
-                                </p>
-                            </figcaption>
-                        </figure>
-                        <figure className="border-left border-primary ps-3 py-2">
-                            <blockquote className="blockquote">
-                                <h5 className="pb-3">
-                                    <i className="fas fa-briefcase me-3" aria-hidden="true"></i>Web developer
-                                </h5>
-                            </blockquote>
-                            <figcaption className="blockquote-footer mb-0">
-                                <p className="font-weight-bold text-body mb-2">
-                                    March 2005 - May 2011 (6 years, 2 Month)
-                                </p>
-                                <p className="mb-0">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit integer posuere.
-                                </p>
-                            </figcaption>
-                        </figure>
-                    </div>
-                    <div className="col-lg-6 text-uppercase">
-                        <h5 className="text-uppercase mb-5">Development Skills</h5>
-                        <p className="font-weight-bold mb-2">JavaScript</p>
+            <section id={"experience"}>
+                
+                <div className="row basic-padding text-center">
+                        <div className="col-md-4 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-5 green-font">Data</h5>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">SQL</p>
                         <div className="progress">
                             <div className="progress-bar" role="progressbar"
-                                 style={{backgroundColor: "#facc62", width: "90%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                 style={{backgroundColor: "#73F843", width: "98%"}}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
-                        <p className="font-weight-bold pt-4 mt-3 mb-2">Css3</p>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Python</p>
                         <div className="progress">
                             <div className="progress-bar" role="progressbar"
-                                 style={{backgroundColor: "#facc62", width: "65%"}}
+                                 style={{backgroundColor: "#73F843", width: "100%"}}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Java</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "95%"}}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Spring</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "97%" }}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Data Management</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "100%" }}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        </div>
+                        <div className="col-md-4 mb-4 mb-md-0">
+                        <h5 className="text-uppercase mb-5 green-font">Front End</h5>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">C#</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "90%"}}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">JavaScript</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "95%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">CSS3</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "83%"}}
                                  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <p className="font-weight-bold pt-4 mt-3 mb-2">HTML5</p>
                         <div className="progress">
                             <div className="progress-bar" role="progressbar"
-                                 style={{backgroundColor: "#facc62", width: "75%"}}
+                                 style={{backgroundColor: "#73F843", width: "89%"}}
                                  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <p className="font-weight-bold pt-4 mt-3 mb-2">Bootstrap</p>
                         <div className="progress">
                             <div className="progress-bar" role="progressbar"
-                                 style={{backgroundColor: "#facc62", width: "45%" }}
+                                 style={{backgroundColor: "#73F843", width: "83%" }}
                                  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        
                         </div>
-                        <p className="font-weight-bold pt-4 mt-3 mb-2">PHP</p>
+                        </div>
+                        <div className="col-md-4 mb-0">
+                        <h5 className="text-uppercase mb-5 green-font">Cloud</h5>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Web Services</p>
                         <div className="progress">
                             <div className="progress-bar" role="progressbar"
-                                 style={{backgroundColor: "#facc62", width: "53%"}}
+                                 style={{backgroundColor: "#73F843", width: "88%" }}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Google Cloud Platform</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "92%" }}
                                  aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Amazon Cloud Platform</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "90%" }}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Data Visualization</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "95%" }}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        <p className="font-weight-bold pt-4 mt-3 mb-2">Enterprise Architecture</p>
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar"
+                                 style={{backgroundColor: "#73F843", width: "93%" }}
+                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        
+                        </div>
+                        </div>
                     </div>
-                </div>
             </section>
         </>
     );
