07fc0e749a44dc8c2fc4f60b4f4b3d2edc687617
Jesse Sites <jesse318s@gmail.com>
2021-06-22 18:12:34 -0500

    Added an MDB library and an accordion to the site. Cleaned the manifest.json.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import './App.css';
 import Header from "./header";
--- src/whoWeAre.jsx
+++ src/whoWeAre.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
 
 function WhoWeAre() {
     return (
@@ -9,6 +10,38 @@ function WhoWeAre() {
                 <p className="text-center mb-5 mx-auto w-responsive green-font">
                     Princigration is a team of dedicated and professional developers having the technical skills to suit any of your needs.
                 </p>
+                <Accordion defaultActiveKey="0">
+                    <Card className="green-font">
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="light" eventKey="0">
+                                Employee
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="0">
+                            <Card.Body>Description</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card className="green-font">
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="light" eventKey="1">
+                                Employee
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="1">
+                            <Card.Body>Description</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card className="green-font">
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="light" eventKey="2">
+                                Employee
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>Description</Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
             </section>
         </>
     );
