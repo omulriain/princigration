09b4ebab08da5e286e53976615222f793c25df8b
Jesse Sites <jesse318s@gmail.com>
2021-06-24 20:47:12 -0500

    Finished accordion, changed admin dashboard

--- src/whoWeAre.jsx
+++ src/whoWeAre.jsx
@@ -13,32 +13,87 @@ function WhoWeAre() {
                 <Accordion defaultActiveKey="0">
                     <Card className="green-font">
                         <Card.Header>
-                            <Accordion.Toggle as={Button} variant="light" eventKey="0">
+                            <Accordion.Toggle as={Button} className="width-fixed" variant="light" eventKey="0">
                                 John Ryan - Chief Data Officer
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
-                            <Card.Body>Description</Card.Body>
+                            <Card.Body>
+                                <ul class="list-group-flush invis-bullet-compensation">
+                                    <li class="list-group-item"><img className="rounded-4 border border-success basic-margin-bottom"
+                                        src="john_ryan.jpg"
+                                        height="250"
+                                        alt="john ryan"
+                                        loading="lazy"
+                                    /></li>
+                                    <li class="list-group-item"><container className="green-font"><h3>Description</h3><p>Contrary to popular belief, Lorem Ipsum is not simply random text.
+                                        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p></container></li>
+                                    <li class="list-group-item"><container className="green-font"><h3>Skills</h3><p>&bull;</p><p>&bull;</p><p>&bull;</p></container></li>
+                                    <li class="list-group-item"><div className="fixed-action-button"><button type="button" className="btn">
+                                        <a href="resume_ryan.pdf" className="green-font"><h3>
+                                            Resume</h3></a></button></div></li>
+                                </ul>
+                            </Card.Body>
                         </Accordion.Collapse>
                     </Card>
-                    <Card className="green-font">
+                    <Card>
                         <Card.Header>
-                            <Accordion.Toggle as={Button} variant="light" eventKey="1">
+                            <Accordion.Toggle as={Button} className="width-fixed" variant="light" eventKey="1">
                                 Jesse Sites - Web Developer
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="1">
-                            <Card.Body>Description</Card.Body>
+                            <Card.Body>
+                                <ul class="list-group-flush invis-bullet-compensation">
+                                    <li class="list-group-item"><img className="rounded-4 border border-success basic-margin-bottom"
+                                        src="jesse_sites.jpg"
+                                        height="250"
+                                        alt="jesse sites"
+                                        loading="lazy"
+                                    /></li>
+                                    <li class="list-group-item"><container className="green-font"><h3>Description</h3><p>
+                                        I am a computer programming student with 3 years of experience in the field of information technology as a student worker
+                                        at Tarleton State University,
+                                        where I have developed my skills as a computer software and programming specialist.
+                                        I interned at TIAER (A Tarleton State University research facility) for 2 years,
+                                        and was a Student Tech for Classroom and Lab Support for 1 year.
+                                        I have also had about 2 years of experience in the service industry before I started college,
+                                        where I have fostered my ability to connect with people in the field.
+                                        I now create web development solutions for my clients utilizing my knowledge gained from Texas State Technical College,
+                                        my new place of study, and from Princigration itself.
+                                    </p></container></li>
+                                    <li class="list-group-item"><container className="green-font"><h3>Skills</h3><p>&bull; HTML</p><p>&bull; CSS</p><p>&bull; JS</p>
+                                        <p>&bull; MDB</p><p>&bull; React</p><p>&bull; Node.js</p><p>&bull; Git/GitHub</p></container></li>
+                                    <li class="list-group-item"><div className="fixed-action-button"><button type="button" className="btn">
+                                        <a href="resume_sites.pdf" className="green-font"><h3>
+                                            Resume</h3></a></button></div></li>
+                                </ul>
+                            </Card.Body>
                         </Accordion.Collapse>
                     </Card>
                     <Card className="green-font">
                         <Card.Header>
-                            <Accordion.Toggle as={Button} variant="light" eventKey="2">
-                                Sam Dawson - Graphic Artist and Digital Media
+                            <Accordion.Toggle as={Button} className="width-fixed" variant="light" eventKey="2">
+                                Sam Dawson - Graphic and Digital Media Artist
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="2">
-                            <Card.Body>Description</Card.Body>
+                            <Card.Body>
+                                <ul class="list-group-flush invis-bullet-compensation">
+                                    <li class="list-group-item"><img className="rounded-4 border border-success basic-margin-bottom"
+                                        src="sam_dawnson.jpg"
+                                        height="250"
+                                        alt="sam dawson"
+                                        loading="lazy"
+                                    /></li>
+                                    <li class="list-group-item"><container className="green-font"><h3>Description</h3><p>Contrary to popular belief, Lorem Ipsum is not simply random text.
+                                        It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p></container></li>
+                                    <li class="list-group-item"><container className="green-font"><h3>Skills</h3><p>&bull;</p><p>&bull;</p><p>&bull;</p></container></li>
+                                    <li class="list-group-item"><div className="fixed-action-button"><button type="button" className="btn">
+                                        <a href="resume_dawson.pdf" className="green-font"><h3>
+                                            Resume</h3></a></button></div></li>
+                                </ul>
+                            </Card.Body>
                         </Accordion.Collapse>
                     </Card>
                 </Accordion>
