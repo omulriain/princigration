13c9421383e3d4efc30e9aac81902c2e7e3a9cd6
Jesse Sites <jesse318s@gmail.com>
2021-07-22 22:21:23 -0500

    Added graphql/amplify todos

--- src/adminHome.jsx
+++ src/adminHome.jsx
@@ -1,10 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Nav } from "react-bootstrap";
 import { MDBCollapse, MDBBtn } from 'mdb-react-ui-kit';
 import { Link } from "react-router-dom";
-import { AmplifySignOut, withAuthenticator} from "@aws-amplify/ui-react";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import { API, graphqlOperation } from 'aws-amplify';
+import { createTodo, deleteTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 function AdminHome() {
+
+    const initialState = { name: '', description: '' }
+    const [formState, setFormState] = useState(initialState)
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        fetchTodos()
+    }, [])
+
+    function setInput(key, value) {
+        setFormState({ ...formState, [key]: value })
+    }
+
+    async function fetchTodos() {
+        try {
+            const todoData = await API.graphql(graphqlOperation(listTodos))
+            const todos = todoData.data.listTodos.items
+            setTodos(todos)
+        } catch (err) { console.log('error fetching todos') }
+    }
+
+    async function addTodo() {
+        try {
+            if (!formState.name || !formState.description) return
+            const todo = { ...formState }
+            setTodos([...todos, todo])
+            setFormState(initialState)
+            await API.graphql(graphqlOperation(createTodo, { input: todo }))
+        } catch (err) {
+            console.log('error creating todo:', err)
+        }
+    }
+    
+
+    const styles = {
+        container: { width: 400, marginTop: 75, marginLeft: 'auto', marginRight: 'auto', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', padding: 20 },
+        todo: { marginBottom: 15, marginTop: 5 },
+        input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
+        todoName: { fontSize: 20, fontWeight: 'bold' },
+        todoDescription: { marginBottom: 0 },
+        button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' },
+        hr: { width: '100%', height: 1 }
+    }
+
     const [showShow, setShowShow] = useState(false);
     const toggleShow = () => setShowShow(!showShow);
     return (
@@ -55,6 +102,32 @@ function AdminHome() {
                     </section>
                 </MDBCollapse>
             </header>
+
+            <div style={styles.container}>
+                <h2>Amplify Todos</h2>
+                <input
+                    onChange={event => setInput('name', event.target.value)}
+                    style={styles.input}
+                    value={formState.name}
+                    placeholder="Name"
+                />
+                <input
+                    onChange={event => setInput('description', event.target.value)}
+                    style={styles.input}
+                    value={formState.description}
+                    placeholder="Description"
+                />
+                <button style={styles.button} onClick={addTodo}>Create Todo</button>
+                {
+                    todos.map((todo, index) => (
+                        <div key={todo.id ? todo.id : index} style={styles.todo}>
+                            <p style={styles.todoName}>{todo.name}</p>
+                            <p style={styles.todoDescription}>{todo.description}</p>
+                        </div>
+                    ))
+                }
+            </div>
+
         </>
     );
 }
