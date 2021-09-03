710cdf50bd7fd1d1fd44d09e16000f65b99dfd1a
Jesse Sites <jesse318s@gmail.com>
2021-09-03 18:12:12 -0500

    Tweaked login page

--- src/adminLogin.jsx
+++ src/adminLogin.jsx
@@ -8,22 +8,23 @@ function AdminLogin() {
     return (
         <>
             {/* login form */}
+            <div className="d-flex justify-content-center">
             <section className="ps-5 pe-5">
-                <h1 className="basic-padding-top">Princigration Administrator Login</h1>
+                <h1 className="font-weight-bold basic-padding-top">Princigration Administrator Login</h1>
                 <Form className="basic-padding-top">
-                    <Form.Group controlId="formAdminLogin">
+                    <Form.Group className="w-75" controlId="formAdminLogin">
                         <Form.Label className="text-dark"><h5>Email address:</h5></Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                             Any Princigration email is safe for use.
                         </Form.Text>
                     </Form.Group>
-                    <Form.Group controlId="formAdminPassword">
+                    <Form.Group className="w-75" controlId="formAdminPassword">
                         <Form.Label className="text-dark pt-2"><h5>Password:</h5></Form.Label>
                         <Form.Control type="password" placeholder="Enter password" />
                     </Form.Group>
                     <Form.Group controlId="formVerifyCheckbox">
-                        <Form.Check className="pt-2" type="checkbox" label="I agree that I am a Princigiration administrator." />
+                        <Form.Check className="pt-2" type="checkbox" label="I agree that I am a Princigration administrator." />
                     </Form.Group>
                     <div className="pt-2">
                         {/* WARNING: temporary unauthorized admin access is granted via submit button without backend authorization process */}
@@ -36,12 +37,13 @@ function AdminLogin() {
                     <div className="pt-2">
                         <Link to="">
                             <Button variant="warning">
-                                Return to Public Site
+                                Return Home
                             </Button>
                         </Link>
                     </div>
                 </Form>
             </section>
+            </div>
         </>
     );
 }
